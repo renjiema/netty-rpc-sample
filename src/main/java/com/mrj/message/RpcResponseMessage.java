53891aa556dc1d7eb1eeba8703a4eb0c67e2c1ac
mrj <376531690@qq.com>
2021-06-15 23:20:22 +0800

    feat(message):Improve RPC request and response message

--- src/main/java/com/mrj/message/RpcResponseMessage.java
+++ src/main/java/com/mrj/message/RpcResponseMessage.java
@@ -1,12 +1,26 @@
 package com.mrj.message;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * RpcResponseMessage
  *
  * @author by mrj
  * @description
  */
+@Setter
+@Getter
 public class RpcResponseMessage extends Message {
+    /**
+     * 返回值
+     */
+    private Object returnValue;
+    /**
+     * 异常值
+     */
+    private Exception exceptionValue;
+
     @Override
     public int getMessageType() {
         return MessageTypeEnum.RPC_RESPONSE_MESSAGE_TYPE.getMessageType();
